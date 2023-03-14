8014555ab12265120667a24d228281d24d85aa29
Ronnie Rios <ronniesmail14@gmail.com>
2023-03-14 10:35:04 -0500

    id page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import { getEventById } from '../../data';
+import Link from 'next/link';
 
 const EventDetails = () => {
+  const id = useRouter(); //set the router metho to a variable
+  const params = id.query.eventId;
+  const foundEvent = getEventById(params);
+
+  if(!foundEvent) { return <p>no event found</p> }
+
   return (
-    <div>EventDetails</div>
+    <div>
+      <h1>{foundEvent.title}</h1>
+      <p>{foundEvent.description}</p>
+    </div>
   )
 }
 
